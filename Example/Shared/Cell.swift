1f1955d7e3cf0205da5d4b5fc7eb029accd6de65
toshi0383 <t.suzuki326@gmail.com>
2017-10-07 22:13:31 +0900

    Update tvOS Example to fetch remote image for thumbnail

--- Example/Shared/Cell.swift
+++ Example/Shared/Cell.swift
@@ -9,16 +9,26 @@
 import UIKit
 
 final class Cell: UICollectionViewCell {
+
     #if os(tvOS)
+    @IBOutlet private weak var imageview: UIImageView!
+    private var task: URLSessionTask?
+    var thumbnailURL: URL! {
+        didSet {
+            self.task?.cancel()
+            self.imageview.image = nil
+            let task = URLSession.shared.dataTask(with: thumbnailURL) { [weak self] data, _, _ in
+                guard let me = self, let data = data else { return }
+                DispatchQueue.main.async { [weak me] in
+                    me?.imageview.image = UIImage(data: data)
+                }
+            }
+            self.task = task
+            task.resume()
+        }
+    }
     override var canBecomeFocused: Bool {
         return true
-    }
-    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
-        if context.nextFocusedView == self {
-            self.backgroundColor = .yellow
-        } else {
-            self.backgroundColor = .white
-        }
     }
     #endif
 }
--- Example/Shared/ViewController.swift
+++ Example/Shared/ViewController.swift
@@ -40,8 +40,11 @@ extension ViewController: UICollectionViewDataSource {
         return 5
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! Cell
         cell.backgroundColor = .white
+        #if os(tvOS)
+            cell.thumbnailURL = URL(string: "https://github.com/toshi0383/assets/raw/master/images/Italy\(indexPath.item + 1).jpg")!
+        #endif
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
@@ -57,14 +60,14 @@ extension ViewController: HorizontalStickyHeaderLayoutDelegate {
         static let headerSize = CGSize(width: 351, height: 38)
         static let itemSize0  = CGSize(width: 447, height: 454)
         static let itemSize1  = CGSize(width: 447, height: 322)
-        static let headerLeft: CGFloat = 60
+        static let spacingForItems: CGFloat = 60
     }
     #elseif os(iOS)
     private enum Const {
         static let headerSize = CGSize(width: 100, height: 38)
         static let itemSize0  = CGSize(width: 50, height: 50)
         static let itemSize1  = CGSize(width: 80, height: 80)
-        static let headerLeft: CGFloat = 8
+        static let spacingForItems: CGFloat = 30
     }
     #endif
     func collectionView(_ collectionView: UICollectionView, hshlSizeForItemAtIndexPath indexPath: IndexPath) -> CGSize {
@@ -78,12 +81,19 @@ extension ViewController: HorizontalStickyHeaderLayoutDelegate {
         return Const.headerSize
     }
     func collectionView(_ collectionView: UICollectionView, hshlHeaderInsetsAtSection section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: Const.headerLeft, bottom: 20, right: 20)
+        return UIEdgeInsets(top: 20, left: 0, bottom: 20, right: Const.spacingForItems)
     }
     func collectionView(_ collectionView: UICollectionView, hshlMinSpacingForCellsAtSection section: Int) -> CGFloat {
-        return 20
+        return Const.spacingForItems
     }
     func collectionView(_ collectionView: UICollectionView, hshlSectionInsetsAtSection section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: section == 4 ? 0 : 20)
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: section == 4 ? 0 : Const.spacingForItems)
+    }
+    func collectionView(_ collectionView: UICollectionView, didUpdateFocusIn context: UICollectionViewFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+
+        self.collectionView.collectionViewLayout.invalidateLayout()
+        coordinator.addCoordinatedAnimations({
+            self.collectionView.layoutIfNeeded()
+        }, completion: nil)
     }
 }

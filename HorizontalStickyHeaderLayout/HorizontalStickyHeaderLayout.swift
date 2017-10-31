1583cd59c938dc064db108a34811ed1f393d6fde
toshi0383 <t.suzuki326@gmail.com>
2017-10-31 11:40:16 +0900

    Fix unwanted pop header

--- HorizontalStickyHeaderLayout/HorizontalStickyHeaderLayout.swift
+++ HorizontalStickyHeaderLayout/HorizontalStickyHeaderLayout.swift
@@ -154,7 +154,7 @@ public final class HorizontalStickyHeaderLayout: UICollectionViewLayout {
             }
             func shouldPopHeader() -> Bool {
                 #if os(tvOS)
-                    if let focusedItemFrame = cv.subviews.flatMap({ $0 as? UICollectionViewCell }).first(where: { $0.isFocused })?.frame {
+                    if let focusedItemFrame = (0..<cv.numberOfItems(inSection: section)).map({ IndexPath(item: $0, section: section) }).flatMap(cv.cellForItem(at:)).first(where: { $0.isFocused })?.frame {
                         let likelyToScrollToLeft = (cv.contentOffset.x > focusedItemFrame.minX)
                         if likelyToScrollToLeft {
                             // Keep header popped regardless header size on left scrolling.
